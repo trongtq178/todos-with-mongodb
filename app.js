d79cf31c8a9758b68e0d8e6b10d43a38d2e899f4
trongtq178 <trongtq178@gmail.com>
2019-08-22 14:55:09 +0700

    create project

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const path = require('path');
+const Joi = require('joi');
+
+const db = require('./db');
+const collection = "todo";
+
+const schema = Joi.object().keys({
+    todo: Joi.string().required()
+});
+
+// create interface
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//get todos
+app.get('/getTodos', (req, res) => {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(documents);
+        }
+    });
+});
+
+//update
+app.put('/:id', (req, res) => {
+    const todoID = req.params.id;
+    const userInput = req.body;
+
+
+    db.getDB().collection(collection).findOneAndUpdate({_id: db.getPrimaryKey(todoID)}, {$set: {todo: userInput.todo}}, {returnOriginal: false}, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+//create
+app.post('/', (req, res, next) => {
+    const userInput = req.body;
+
+    Joi.validate(userInput, schema, (err, result) => {
+        if(err){
+            debugger
+            const error = new Error('Invalid Input');
+            error.status = 400;
+            next(error);
+        }else{
+            db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+                if(err){
+                    const error = new Error('Failed to insert Todo Document');
+                    error.status = 400;
+                    next(error);
+                }else{
+                    res.json({result: result, document: result.ops[0], msg: "Successfully inserted Todo!!!", error: null});
+                }
+            });
+        }
+    });
+})
+
+//delete
+app.delete('/:id', (req, res) => {
+    const todoID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(todoID)}, (err, result) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(result);
+        }
+    });
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status).json({
+        error: {
+            message: err.message
+        }
+    });
+});
+
+//connect with mongodb
+db.connect((err) => {
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }else{
+        app.listen(3000, () => {
+            console.log('connect to database, app listening on port 3000');
+        });
+    }
+});
---
+++ readme.txt
@@ -0,0 +1,5 @@
+Tải các dependencies cần thiết:
+    npm install
+Chạy server:
+    node app
+Mở đường dẫn http://localhost:3000
